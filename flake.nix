48018e5356d8fc23ec41de00e198a1db339d1b2b
levivannoort <levivnoort@gmail.com>
2025-09-29 23:08:16 +0200

    feat: add darwin configuration and update flake inputs to version 25.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-25.05";
     };
 
     home-manager = {
@@ -12,7 +12,7 @@
     };
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -22,18 +22,20 @@
     host = "sld";
     user = "levi";
   in {
-    darwinConfigurations."${host}" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [
-        ./hosts/${host}/darwin.nix
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${user} = import "./home-manager/home-manager.nix";
-          users.users.${user}.home = "/Users/${user}";
-        }
-      ];
+    darwinConfigurations = {
+      "${host}" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./hosts/${host}/darwin.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.${user} = import "./home-manager/home-manager.nix";
+            users.users.${user}.home = "/Users/${user}";
+          }
+        ];
+      };
     };
   };
 }
